b35a1504618a48adf45fb837f81b11a8cd16c798
cRohrer82 <80014660+cRohrer82@users.noreply.github.com>
2021-03-25 12:52:18 -0400

    Add Game Class
    Add Game Class file

---
+++ Class_Game.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Christopher Rohrer
+"""
+import random as R
+
+class Game:
+	'Represents a game created by the player'
+	
+	def __init__(self, player, max):
+		self.player = player
+		self.max = max
+		self.goal = goal
+		
+	def makeMessage(self):
+		'code'
+		
+	def findGoodNumber(self, max):
+		'code'
+		
+	def findGoal(self, max):
+		'code'
--- Class_Player.py
+++ Class_Player.py
@@ -1,1 +1,16 @@
-Player Class
+# -*- coding: utf-8 -*-
+"""
+@author: Christopher Rohrer
+"""
+
+class Player:
+	'Represents the player'
+	
+	def __init__(self, name):
+		self.name = name
+		
+	def makeGuess(self):
+		'code'
+		
+	def makeGame(self):
+		'code'
