05b733af4f8b8074cfdf0e4afadd2b6ff10901ad
cRohrer82 <80014660+cRohrer82@users.noreply.github.com>
2021-03-30 08:56:57 -0400

    Write 'modified' code

--- Classes.py
+++ Classes.py
@@ -21,15 +21,29 @@ class Game:
 		self.goal = self.findGoal()
 		self.turns = self.findGoodNumber()
 		
+	def __init__(self, max, rounds):
+		# self.name = name
+		# self.player = player
+		self.max = self.parseMax(max)
+		self.rounds = self.parseRounds(rounds)
+		self.goal = self.findGoal()
+		self.turns = self.findGoodNumber()
+		
 	def parseMax(self, max):
 		while M.isnan(max) or max < 2 or max > 2000000000:
 			max = int(input("Please choose a whole number that is greater than 1 and less than 2 billion!... "))
 			
 		return max
+		
+	def parseRounds(self, rounds):
+		while M.isnan(rounds) or rounds < 1 or rounds > 10:
+			rounds = int(input("Please enter a number from 1 to 10... "))
+			
+		return rounds
 			
 	def findGoal(self):
 		intGoal = R.randrange(1, self.max + 1)
-		print(intGoal)
+		print(intGoal) #Comment line out BEFORE publishing. For debug use ONLY!
 		
 		return intGoal
 		
@@ -43,24 +57,25 @@ class Game:
 			
 		return intTurns
 				
-	def guessNumber(self):
+	def guessNumber(self, rounds = 1):
 		intGuesses = 0
 		intGuess = 0
-		while intGuess != self.goal:
-			intGuess = int(input("Choose a number from 1 to game max"))
-			if M.isnan(intGuess) or intGuess < 1 or intGuess > self.max:
-				print("Please guess whole numbers between 1 and the game maximum of ", self.max, "!\nPress 'Enter' to continue.")
-				input()
-				continue
+		for x in range(rounds):
+			while intGuess != self.goal:
+				intGuess = int(input("Choose a number from 1 to game max... "))
+				if M.isnan(intGuess) or intGuess < 1 or intGuess > self.max:
+					print("Please guess whole numbers between 1 and the game maximum of ", self.max, "!\nPress 'Enter' to continue.")
+					input()
+					continue
+					
+				intGuesses += 1
 				
-			intGuesses += 1
-			
-			if intGuess > self.goal:
-				print("Nope, it's less than that")
-			if intGuess < self.goal:
-				print("Nope, it's greater than that")
-				
-		self.endGame(intGuesses)
+				if intGuess > self.goal:
+					print("Nope, it's less than that")
+				if intGuess < self.goal:
+					print("Nope, it's greater than that")
+					
+			self.endGame(intGuesses)
 		
 	def endGame(self, guesses):
 		print("That's it! Congratulations!\nIt took you ", guesses, " turns.")
@@ -70,12 +85,5 @@ class Game:
 			print("Try to get it less than ", guesses, " turns next time")
 		if guesses == 1:
 			print("Wow! That's great!")
+			print()
 		
-#=======================
-	# intHigh = 2
-	# intRounds = 1
-	# while intHigh < randMax:
-		# intRounds += 1
-		# intHigh *= 2
-		
-	# return intRounds
--- TestMain.py
+++ TestMain.py
@@ -36,17 +36,28 @@ def mainMenu():
 		import Classes as c
 		Game = c.Game(100)
 		Game.guessNumber()
+		del Game
 		mainMenu()
 
 	elif charFormat == "2":
 		print("New Modified Game")
-		import Modified as M
-		M.playGame()
+		import Classes as c
+		intMaximum = 0
+		intMaximum = int(input("What do you want the maximum to be?... "))
+		Game = c.Game(intMaximum)
+		Game.guessNumber()
+		del Game
 		mainMenu()
 
 	elif charFormat =="3":
-		import Simulator as S
-		S.playGame()
+		import Classes as c
+		intMaximum = 0
+		intRounds = 0
+		intMaximum = int(input("What is the maximum for the simulations?... "))
+		intRounds = int(input("How many simulations do you want to run?... "))
+		Game = c.Game(intMaximum, intRounds)
+		Game.guessNumber(intRounds)
+		del Game
 		mainMenu()
 
 	elif charFormat == "4":
@@ -60,4 +71,69 @@ def mainMenu():
 		mainMenu()
 
 mainMenu()
+
+#========================
+
+# def simGame(gameNum, maxGoal, currentGoal):
+	# intMax = maxGoal
+	# intMin = 0
+	# intTry = 1
+	# intGuess = 0
+	
+	# print ("Game ", gameNum, ", Game Range ", maxGoal, ", Goal Number ", currentGoal)
+	
+	# while intGuess != currentGoal:
+		# intRange = int(intMax - intMin)
+		# intGuess = int(intMin + (intRange / 2))
+		# if intGuess < currentGoal:
+			# print("Guess ", intTry, " -> ", intGuess, "too low")
+			# intMin = intGuess
+		# elif intGuess > currentGoal:
+			# print("Guess ", intTry, " -> ", intGuess, "too high")
+			# intMax = intGuess
+		# else:
+			# print("Guess ", intTry, " -> ", intGuess, " was correct")
+		
+		# intTry += 1
+			
+
+# def simRounds():
+	# print("How many times do you want the computer to run this simulation?")
+	
+	# intLoops = 0
+	# while intLoops < 1 or intLoops > 10:
+		# try:
+			# intLoops = int(input("Minimum 1, Maximum 10 ... "))
+		# except:
+			# print("You need to pick a number between 1 and 10")
+			# input()
+			# simRounds()
+	# print()
+	
+	# return intLoops
+
+# def playGame():
+	# charAgain = "Y"
+	# intHigh = 2
+	# intRounds = 1
+	# intGame = 0
+	# import Game as G
+	# import Modified as M
+	
+	# while charAgain == "Y":
+		# charAgain = "Z"
+		# import random as R
+		# intMod = M.modGame()
+		# intLoops = simRounds()
+		# while intLoops > 0:
+			# intLoops -= 1
+			# intGame += 1
+			# intRand = R.randrange(1, intMod)
+			# simGame(intGame, intMod, intRand)
+			# print()
+			
+		# intGame = 0
+		# print("Would you like to run another simulation?")
+		# charAgain = G.yOrN()
+
 	
